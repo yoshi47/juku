853682a514dc00eb2c85454b7877b0862e87cef3
yoshi47 <yoshix47@gmail.com>
2023-06-05 23:30:00 +0900

    herokuにあげれるように

--- backend/juku/settings.py
+++ backend/juku/settings.py
@@ -11,21 +11,28 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# todo envファイルをつくる
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!h0+p%ogu2+2_gtrb8ma-9p0z%kyv)pt0(p7ckf#v!$y^=f$a0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# todo *をやめる
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +82,7 @@ WSGI_APPLICATION = 'juku.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': env.db(),
     }
 }
 
@@ -121,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals(), databases=False)
