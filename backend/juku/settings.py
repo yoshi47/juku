a3446fe445d09f47bb398049d103e0691b826fb6
yoshi47 <yoshix47@gmail.com>
2023-06-06 10:14:01 +0900

    secret_keyをenvファイルに書いた

--- backend/juku/settings.py
+++ backend/juku/settings.py
@@ -24,15 +24,13 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# todo envファイルをつくる
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!h0+p%ogu2+2_gtrb8ma-9p0z%kyv)pt0(p7ckf#v!$y^=f$a0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-# todo *をやめる
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com', 'juku-django-1']
 
 
 # Application definition
