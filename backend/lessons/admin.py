f9b2afef07655ab91c9ea10945c97e6da34f85e6
yoshi47 <yoshix47@gmail.com>
2023-06-06 13:33:29 +0900

    add lessons model

--- backend/lessons/admin.py
+++ backend/lessons/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Lesson, Subject
 # Register your models here.
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
