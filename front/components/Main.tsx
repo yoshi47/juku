1c887f4f04f355ee9329e4992fc00827a9b14f61
yoshi47 <yoshix47@gmail.com>
2023-06-07 11:37:05 +0900

    add header and footer

---
+++ front/components/Main.tsx
@@ -0,0 +1,7 @@
+export function Main({ children }: { children: React.ReactNode }) {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {children}
+        </main>
+    )
+}
