9c6d26d6bb06b378727fb967cc6e9f6040aca4e6
yoshi47 <yoshix47@gmail.com>
2023-06-15 00:34:34 +0900

    :sparkles: 登録ボタン追加

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -19,14 +19,12 @@ export function Header() {
                     <Link href="/students" className="mr-5 hover:text-gray-900">生徒</Link>
                     <Link href="/teachers" className="mr-5 hover:text-gray-900">先生</Link>
                 </nav>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <Link href="" className="mr-5 hover:text-gray-900">新規先生</Link>
+                    <Link href="" className="mr-5 hover:text-gray-900">新規生徒</Link>
+                    <Link href="" className="mr-5 hover:text-gray-900">授業登録</Link>
+                </nav>
                 <LoginBtn />
-                <button
-                    className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                         strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                </button>
             </div>
         </header>
     )
