078dd7a6ab338c2fc3c6176d366da20631cb8e5b
yoshi47 <yoshix47@gmail.com>
2023-06-23 15:24:11 +0900

    :recycle: useStateを使ったフォーム(先生)

--- front/app/teachers/new/page.tsx
+++ front/app/teachers/new/page.tsx
@@ -1,24 +1,32 @@
 "use client";
 import process from "process";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
+import {ChangeEvent, FormEvent, useState} from "react";
 
 export default function AddTeacher() {
     const router = useRouter()
+    const [teacher_data, setTeacherData] = useState({
+        "username": "",
+        "password": "te",
+        "last_name": "",
+        "first_name": "",
+        "email": "",
+        "text": "",
+        "user_type": "teacher"
+    });
 
-    async function handleSubmit(event: any) {
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const {name, value} = event.target;
+        setTeacherData({
+            ...teacher_data,
+            [name]: value,
+        });
+    }
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
-        const teacherData = {
-            username: event.target.username.value,
-            password: "te",
-            last_name: event.target.last_name.value,
-            first_name: event.target.first_name.value,
-            email: event.target.email.value,
-            text: "",
-            user_type: "teacher",
-        }
-
-        const JSONteacherData = JSON.stringify(teacherData)
+        const JSONteacherData = JSON.stringify(teacher_data)
         // console.log(JSONteacherData)
 
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/teachers/`, {
@@ -33,30 +41,31 @@ export default function AddTeacher() {
         // alert(result.message)
         router.push("/teachers")
     }
+
     return (
         <div className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
             <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">新規先生登録</h2>
 
             <form onSubmit={handleSubmit}>
-            {/*<form>*/}
                 <div className="grid grid-cols-1 gap-6 mt-4">
                     <div>
                         <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Username</label>
-                        <input id="username" type="text" name="username"
+                        <input id="username" type="text" name="username" value={teacher_data.username}
+                               onChange={handleChange}
                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                     </div>
 
                     <div className="flex gap-4 mb-2">
                         <div className=" relative ">
                             <label className="text-gray-700 dark:text-gray-200" htmlFor="last-name">姓</label>
-                            <input type="text" id="last-name"
+                            <input type="text" id="last-name" value={teacher_data.last_name} onChange={handleChange}
                                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                                    name="last_name" placeholder="山田"/>
                         </div>
 
                         <div className=" relative ">
                             <label className="text-gray-700 dark:text-gray-200" htmlFor="first-name">名</label>
-                            <input type="text" id="first-name"
+                            <input type="text" id="first-name" value={teacher_data.first_name} onChange={handleChange}
                                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                                    name="first_name" placeholder="太郎"/>
                         </div>
@@ -64,7 +73,7 @@ export default function AddTeacher() {
 
                     <div>
                         <label className="text-gray-700 dark:text-gray-200" htmlFor="email">Email Address</label>
-                        <input id="email" type="email" name="email"
+                        <input id="email" type="email" name="email" value={teacher_data.email} onChange={handleChange}
                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                     </div>
 
@@ -90,4 +99,5 @@ export default function AddTeacher() {
                 </div>
             </form>
         </div>
-    )}
+    )
+}
